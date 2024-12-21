3c66d8ddb85b2ae048ca944faa0e9f181b58864c
Vijay tanmay <vijay@Vijays-MacBook-Air.local>
2024-12-21 17:24:52 +0530

    gunicorn update

--- app.py
+++ app.py
@@ -46,4 +46,6 @@ def test3():
   return render_template('test3.html')
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  # app.run(debug=True)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host="0.0.0.0", port=port)
